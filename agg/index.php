ab8aa2a634cf58d7b559a9e14945656669acda5a
sankalp20436 <sankalpvarma25@gmail.com>
2022-05-02 15:56:01 +0530

    DD

---
+++ agg/index.php
@@ -0,0 +1,307 @@
+<!doctype html>
+<html class="no-js" lang="en">
+
+<head>
+  <meta charset="utf-8">
+
+  <title>ANN-data</title></title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="css/style.css">
+
+  <link rel="stylesheet" href="css/styles.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;400;700;900&family=Ubuntu:wght@300&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.7.1/slick.css" />
+  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.7.1/slick-theme.css" />
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet">
+</head>
+
+<body>
+  <header class="site-header">
+    <div class="container">
+      <div class="header-content">
+        <div class="top-menu  scroll-reveal">
+          <div class="menu">
+            <div class="title-bar" data-responsive-toggle="main-nav" data-hide-for="medium">
+              <button class="menu-icon dark" type="button" data-toggle="main-nav"></button>
+              <div class="title-bar-title">Menu</div>
+            </div>
+            <nav id="main-nav" data-animate="menu-in menu-out">
+              <ul class="main-navigation">
+                <li><a class="m-active" href="#" data-text="Home">Home</a></li>
+                <li><a class="m-anim" href="#specials-login" data-text="login">Register/Login</a></li>
+                <li><a class="m-anim" href="#about-us" data-text="About">About</a></li>
+                <li><a class="m-anim" href="#contact-us" data-text="Contact">Contact</a></li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="slider">
+      <div class="a-slide slide1">
+        <div class="container">
+          <div class="bottom-section scroll-reveal" data-origin="right" data-distance="20%">
+            <h1 class="header-txt"> Agriculture is our wisest pursuit because it will in the end contribute most to real wealth, good morals, and happiness.</h1>
+          </div>
+        </div>
+      </div>
+      <div class="a-slide slide2">
+        <div class="container">
+          <div class="bottom-section">
+            <h1 class="header-txt">If you tickle the earth with a hoe she laughs with a harvest.</h1>
+
+          </div>
+          <!--.bottom-section-->
+        </div>
+      </div>
+      <!--.bottom-section-->
+    </div>
+
+
+  </header>
+
+  <section class="category-icons">
+    <div class="container">
+      <div class="yellow-content">
+
+      <dev class="box col-lg-4">
+
+          <img class="logo" src="assets/img/snap.png" />
+        <h3>Easy to use.</h3>
+        <p>So easy to use that you fall in love with it.</p>
+      </dev>
+
+      <dev class="box col-lg-4"><img class="logo"  src="assets/img/people.png" />
+        <h3>Connects people </h3>
+        <p>Connects to people and make work easier.</p>
+      </dev>
+
+      <dev class="box col-lg-4">
+        <img class="logo"  src="assets/img/back.png" />
+        <h3>Guaranteed to work</h3>
+        <p>We take responsibilty to get you the best deal.</p>
+      </dev>
+
+    </dev>
+  </div>
+  <!--.yellow-content-->
+</div>
+<!--.container-->
+</section>
+
+
+
+
+  </section>
+
+
+  <!-- login/register -->
+
+  <section id="specials-login" class="month-specials" data-scroll-reveal="enter from the bottom after 0.9s">
+
+    <div class="container">
+      <h1 class="header-txt">LOGIN/REGISTER</h1>
+
+      <div class="divider">
+        <?xml version="1.0" encoding="utf-8"?>
+        <!-- Generator: Adobe Illustrator 21.1.0, SVG Export Plug-In . SVG Version: 6.00 Build 0)  -->
+        <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 1930 255.5" style="enable-background:new 0 0 1930 255.5;" xml:space="preserve">
+          <style type="text/css">
+            .st0 {
+              fill: none;
+              stroke: #3c3c3c;
+              stroke-width: 10;
+              stroke-linecap: square;
+              stroke-miterlimit: 10;
+            }
+          </style>
+          <polyline class="st0" points="1224,171.8 1181.3,171.8 1139.2,129.6 1065,203.9 970.5,110.4 876,203.6 801.8,129.4 759.7,171.5
+                                  717,171.5 " />
+          <polyline class="st0" points="5,131.5 757.3,131.5 801.8,176.1 885.9,91.9 868.3,74.2 831.5,111 870.4,149.9 970.2,50.2 1070,149.9
+                                  1108.8,111 1072.1,74.2 1054.4,91.9 1138.5,176.1 1183.1,131.5 1925,131.5 " />
+          <rect x="921.9" y="26.7" transform="matrix(0.7071 -0.7071 0.7071 0.7071 231.426 707.2043)" class="st0" width="95" height="95" />
+          <rect x="921.9" y="99.7" transform="matrix(0.7071 -0.7071 0.7071 0.7071 179.8072 728.5855)" class="st0" width="95" height="95" />
+          <rect x="940.3" y="178.1" transform="matrix(0.7071 -0.7071 0.7071 0.7071 137.3893 746.1556)" class="st0" width="58.2" height="58.2" />
+        </svg>
+      </div>
+      <!--.divider-->
+      <section class="hero-section">
+        <div class="card-grid">
+          <a class="card" >
+            <div class="card__background"
+              style="background-image: url(https://previews.123rf.com/images/lovelyday12/lovelyday121806/lovelyday12180600022/103597375-a-farmer-using-tablet-computer-checking-data-of-agriculture-in-a-sugarcane-plantation.jpg)"></div>
+
+          </a>
+          <a class="card" href="../AgroCulture\index.php">
+            <div class="card__background" style="background-image: url(https://m.economictimes.com/thumb/msid-69702930,width-1200,height-900,resizemode-4,imgsize-574454/farmer.jpg)"></div>
+            <div class="card__content">
+              <h3 class="card__heading">FARMERS</h3>
+              <p class="card__category">LOGIN/REGISTRAION</p>
+
+            </div>
+            <a class="card" href="../AgroCulture\index.php">
+              <div class="card__background" style="background-image: url(https://www.financialexpress.com/wp-content/uploads/2019/12/online-shopping-trends-small.jpg)"></div>
+              <div class="card__content">
+                <h3 class="card__heading">CUSTOMERS</h3>
+                <p class="card__category">LOGIN/REGISTRAION</p>
+              </div>
+            </a>
+          </a>
+          <a class="card" >
+            <div class="card__background" style="background-image: url(https://borgenproject.org/wp-content/uploads/25811566488_bf1b98a05c_z-1.jpg)"></div>
+          </a>
+
+      </section>
+    </div>
+    <!--.container-->
+  </section>
+  <!--.month-specials-->
+  <section id="about-us" class="about">
+      <div class="container">
+          <div class="about-content">
+              <h1 class="header-txt scroll-reveal">About us</h1>
+              <div class="divider scroll-reveal">
+                  <?xml version="1.0" encoding="utf-8"?>
+                  <!-- Generator: Adobe Illustrator 21.1.0, SVG Export Plug-In . SVG Version: 6.00 Build 0)  -->
+                  <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 1930 255.5" style="enable-background:new 0 0 1930 255.5;" xml:space="preserve">
+                              <style type="text/css">
+                                  .st0{fill:none;stroke:#3c3c3c;stroke-width:10;stroke-linecap:square;stroke-miterlimit:10;}
+                              </style>
+                              <polyline class="st0" points="1224,171.8 1181.3,171.8 1139.2,129.6 1065,203.9 970.5,110.4 876,203.6 801.8,129.4 759.7,171.5
+                                  717,171.5 "/>
+                              <polyline class="st0" points="5,131.5 757.3,131.5 801.8,176.1 885.9,91.9 868.3,74.2 831.5,111 870.4,149.9 970.2,50.2 1070,149.9
+                                  1108.8,111 1072.1,74.2 1054.4,91.9 1138.5,176.1 1183.1,131.5 1925,131.5 "/>
+                              <rect x="921.9" y="26.7" transform="matrix(0.7071 -0.7071 0.7071 0.7071 231.426 707.2043)" class="st0" width="95" height="95"/>
+                              <rect x="921.9" y="99.7" transform="matrix(0.7071 -0.7071 0.7071 0.7071 179.8072 728.5855)" class="st0" width="95" height="95"/>
+                              <rect x="940.3" y="178.1" transform="matrix(0.7071 -0.7071 0.7071 0.7071 137.3893 746.1556)" class="st0" width="58.2" height="58.2"/>
+                              </svg>
+              </div>
+              <!--.divider-->
+              <p >The purpose of ANN-data is to automate the existing manual system by the help of  full-fledged computer software, fulfilling their requirements, so that their valuable data/information can be stored for a longer period with easy accessing and manipulation of the same.</p>
+
+          </div>
+      </div>
+      <!--.container-->
+  </section>
+  <!--.about-->
+
+
+  <a href="#0" class="cd-top">Top</a>
+
+  <footer id="contact-us">
+    <div class="container">
+      <div class="footer-content">
+        <h1 class="header-txt scroll-reveal">Contact</h1>
+        <div class="divider scroll-reveal">
+          <?xml version="1.0" encoding="utf-8"?>
+          <!-- Generator: Adobe Illustrator 21.1.0, SVG Export Plug-In . SVG Version: 6.00 Build 0)  -->
+          <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 1930 255.5" style="enable-background:new 0 0 1930 255.5;" xml:space="preserve">
+            <style type="text/css">
+              .st0 {
+                fill: none;
+                stroke: #3c3c3c;
+                stroke-width: 10;
+                stroke-linecap: square;
+                stroke-miterlimit: 10;
+              }
+            </style>
+            <polyline class="st0" points="1224,171.8 1181.3,171.8 1139.2,129.6 1065,203.9 970.5,110.4 876,203.6 801.8,129.4 759.7,171.5
+                                    717,171.5 " />
+            <polyline class="st0" points="5,131.5 757.3,131.5 801.8,176.1 885.9,91.9 868.3,74.2 831.5,111 870.4,149.9 970.2,50.2 1070,149.9
+                                    1108.8,111 1072.1,74.2 1054.4,91.9 1138.5,176.1 1183.1,131.5 1925,131.5 " />
+            <rect x="921.9" y="26.7" transform="matrix(0.7071 -0.7071 0.7071 0.7071 231.426 707.2043)" class="st0" width="95" height="95" />
+            <rect x="921.9" y="99.7" transform="matrix(0.7071 -0.7071 0.7071 0.7071 179.8072 728.5855)" class="st0" width="95" height="95" />
+            <rect x="940.3" y="178.1" transform="matrix(0.7071 -0.7071 0.7071 0.7071 137.3893 746.1556)" class="st0" width="58.2" height="58.2" />
+          </svg>
+        </div>
+        <!--.divider-->
+        <div class="contact-info scroll-reveal">
+          <div class="info-address">
+            <h3>agriculture mangement system</h3>
+          </div>
+          <div class="reservations">
+
+
+          </div>
+        </div>
+        <!--.contact-info-->
+        <div class="contact-form scroll-reveal" data-origin="bottom" data-distance="20%">
+          <h3>Contact us</h3>
+          <form>
+            <input type="text" name="name" placeholder="Name">
+            <input type="email" name="email" placeholder="Email">
+            <textarea placeholder="Message"></textarea>
+            <button class="send-form">Send</button>
+          </form>
+        </div>
+        <!--.contact-form-->
+        <div class="social-icons scroll-reveal" data-duration="1500">
+          <div class="fb-i">
+
+            <?xml version="1.0" encoding="iso-8859-1"?>
+            <!-- Generator: Adobe Illustrator 19.1.0, SVG Export Plug-In . SVG Version: 6.00 Build 0)  -->
+            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" viewBox="0 0 288.861 288.861" style="enable-background:new 0 0 288.861 288.861;" xml:space="preserve"
+              width="30px" height="30px">
+              <path
+                d="M167.172,288.861h-62.16V159.347H70.769v-59.48h34.242v-33.4C105.011,35.804,124.195,0,178.284,0   c19.068,0,33.066,1.787,33.651,1.864l5.739,0.746l-1.382,55.663l-6.324-0.058c-0.013,0-14.223-0.135-29.724-0.135   c-11.536,0-13.066,2.847-13.066,14.171v27.629h50.913l-2.821,59.48h-48.086v129.501H167.172z M117.858,276.007h36.453V146.5h48.677   l1.607-33.779h-50.284V72.238c0-13.368,3.078-27.025,25.919-27.025c9.178,0,17.899,0.045,23.509,0.09l0.778-31.292   c-5.675-0.508-15.116-1.157-26.247-1.157c-44.544,0-60.419,27.693-60.419,53.613v46.254H83.61V146.5h34.242v129.507H117.858z"
+                fill="#3c3c3c" />
+            </svg>
+          </div>
+          <div class="insta-i">
+            <?xml version="1.0" encoding="iso-8859-1"?>
+            <!-- Generator: Adobe Illustrator 19.0.0, SVG Export Plug-In . SVG Version: 6.00 Build 0)  -->
+            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" viewBox="0 0 48 48" style="enable-background:new 0 0 48 48;" xml:space="preserve" width="30px" height="30px">
+              <path
+                d="M40.4,5.5H35.6c-1.434,0-2.6,1.166-2.6,2.6V12.9c0,1.434,1.166,2.6,2.6,2.6H40.4c1.434,0,2.6-1.166,2.6-2.6V8.1    C43,6.666,41.834,5.5,40.4,5.5z M41,12.9c0,0.331-0.269,0.6-0.6,0.6H35.6c-0.331,0-0.6-0.269-0.6-0.6V8.1    c0-0.331,0.269-0.6,0.6-0.6H40.4c0.331,0,0.6,0.269,0.6,0.6V12.9z"
+                fill="#3c3c3c" />
+              <path
+                d="M37.8,0.5H10.2C4.576,0.5,0,5.089,0,10.731V17.5v2v17.77C0,42.911,4.576,47.5,10.2,47.5h27.6    c5.624,0,10.2-4.589,10.2-10.23V19.5v-2v-6.769C48,5.089,43.424,0.5,37.8,0.5z M46,37.27c0,4.538-3.679,8.23-8.2,8.23H10.2    c-4.522,0-8.2-3.692-8.2-8.23V19.5h12.221C11.62,22.043,10,25.584,10,29.5c0,7.72,6.28,14,14,14s14-6.28,14-14    c0-3.916-1.62-7.457-4.221-10H46V37.27z M36,29.5c0,6.617-5.383,12-12,12s-12-5.383-12-12s5.383-12,12-12S36,22.883,36,29.5z     M31.174,17.5c-2.101-1.261-4.55-2-7.174-2s-5.073,0.739-7.174,2H2v-6.769C2,6.192,5.679,2.5,10.2,2.5h27.6    c4.521,0,8.2,3.692,8.2,8.231V17.5H31.174z"
+                fill="#3c3c3c" />
+              <path d="M15,29.5c0,4.962,4.037,9,9,9s9-4.038,9-9s-4.037-9-9-9S15,24.538,15,29.5z M31,29.5c0,3.86-3.141,7-7,7s-7-3.14-7-7    s3.141-7,7-7S31,25.64,31,29.5z" fill="#3c3c3c" />
+
+            </svg>
+          </div>
+        </div>
+        <!--.social-icons-->
+      </div>
+      <!--.footer-content-->
+
+    </div>
+    <!--.container-->
+  </footer>
+
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/what-input/5.0.2/what-input.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/foundation/6.4.3/js/foundation.js"></script>
+  <script src="js/app.js"></script>
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.7.1/slick.js"></script>
+  <script src="https://unpkg.com/scrollreveal/dist/scrollreveal.min.js"></script>
+
+  <script type="text/javascript">
+    $('.slider').slick({
+      infinite: true,
+      autoplay: true,
+      autoplaySpeed: 3500,
+      arrows: false,
+      fade: true,
+      cssEase: 'linear'
+    });
+  </script>
+
+  <script>
+    $(window).on('load', function() {
+      $("#preloader").fadeOut();
+    });
+  </script>
+
+
+
+</body>
+
+</html>
