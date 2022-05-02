c79ef4d82e8f2cacde87019160ca6aa270dc699f
sankalp20436 <sankalpvarma25@gmail.com>
2022-05-02 15:57:03 +0530

    ADD

---
+++ AgroCulture/essentials/essentials/techupdate.php
@@ -0,0 +1,419 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <title>techUpdate</title>
+    <style>
+        body {
+            background-image: url("images (4).jpg");
+            background-size: cover;
+        }
+
+        img.img-fluid {
+            height: 50%;
+            width: 100%;
+            z-index: -1;
+        }
+
+        /*.adit{
+            background-image: url("depositphotos_9292507-stock-photo-spring-background.jpg");
+            background-size: cover;
+            text-align: center;
+            font-size: xx-large;
+
+          }*/
+        .carousel-item {
+            height: 700px;
+        }
+
+        .thanos {
+            font-size: x-large;
+            color: aliceblue;
+            text-align: left;
+        }
+
+        .a li {
+            color: aliceblue;
+            font-size: x-large;
+        }
+
+        .nav nav-tabs {
+            background-color: hsla(106, 92%, 20%, 0.404);
+        }
+
+        .dub {
+            color: aliceblue;
+            text-align: center;
+            font-size: x-large;
+        }
+
+        .b {
+            font-size: xx-large;
+            color: yellow;
+        }
+
+        .ib {
+            font-size: x-large;
+            color: orange;
+        }
+
+        #la {
+            box-shadow: inset;
+        }
+
+        .navbar-brand {
+            font-size: xx-large;
+            color: white;
+        }
+
+        footer {
+            text-align: left;
+            padding: 3px;
+            background-color: rgb(231, 217, 23);
+            color: white;
+
+
+        }
+
+        footer a {
+            text-decoration: none;
+
+
+        }
+
+        .prev {
+            margin: 3px 3px 3px 1px;
+            border-radius: 30px;
+        }
+
+        .carousel-control-prev-icon,
+        .carousel-control-next-icon {
+            height: 100px;
+            width: 100px;
+            outline: black;
+            background-color: rgba(0, 0, 0, 0.3);
+            background-size: 100%, 100%;
+            border-radius: 50%;
+            border: 1px solid black;
+        }
+
+        .nay {
+            border-style: dotted;
+            border-radius: 30px;
+            border-color: black;
+            background-color: rgba(2, 2, 2, 0.5);
+
+        }
+
+        .s1 {
+            color: aliceblue;
+        }
+
+        .ada {
+            font-size: xx-large;
+            color: aliceblue;
+            text-align: center;
+        }
+
+        .logo1 {
+            height: 400px;
+            width: 600px;
+            margin-left: 350px;
+
+        }
+
+        /*.center {
+            display: block;
+            margin-left: auto;
+            margin-right: auto;
+            width: 200px;
+            height: 100px;
+          }*/
+        .para1 {
+            color: aliceblue;
+        }
+
+        .para1 li {
+            color: aliceblue;
+        }
+
+        ul {
+            list-style-type: none;
+            margin: 0;
+            padding: 0;
+            overflow: hidden;
+            background-color: #333;
+            position: -webkit-sticky;
+            /* Safari */
+            position: sticky;
+            top: 0;
+        }
+
+        li {
+            float: left;
+        }
+
+        li a {
+            display: block;
+            color: white;
+            text-align: center;
+            padding: 14px 16px;
+            text-decoration: none;
+        }
+
+        li a:hover {
+            background-color: #111;
+        }
+
+        .active {
+            background-color: #4CAF50;
+        }
+
+        .clear {
+            clear: both;
+        }
+    </style>
+</head>
+
+<body>
+    <img src="shutterstock_1258961086.jpg" class="img-fluid" alt="wait">
+    <ul>
+      <a class="navbar-brand">किसान</a>
+    </ul>
+
+
+
+    <div class="container">
+        <p class="ada"><strong>EMERGING DRONES</strong></p>
+        <br>
+        <br>
+        <img src="DRONE.jpg" class="logo1" class="center">
+        <br>
+        <br>
+        <div class="para1">
+            <p><strong>Capturing data from agriculture drone takes place as in the following stages: </strong></p>
+            <br>
+            <ol type="1">
+                <li>Analyzing the area: This identifies the territory being tested. Therefore, the first step includes
+                    establishing a boundary, analyses of the area, and then finally, uploading the technical GPS
+                    information into the drone's navigation system.
+                </li>
+                <li>Using Autonomous Drones: Since Unmanned aerial vehicles (UAVs) are independent, they enter flight
+                    patterns into their already established system to collect required data. </li>
+                <li> Uploading the data: After capturing all the required data through sensors such as the multispectral
+                    sensor/RGB sensor, it is processed through numerous software for further analysis and
+                    interpretation.</li>
+                <li>Output: After collecting the data, they format it so that farmers can understand the data with no
+                    hassle, bringing them a step closer to precision farming. 3D mapping or Photogrammetry are popular
+                    methods to display extensive data collected.</li>
+
+                </ul>
+                <br>
+                <br>
+        </div>
+        <div class="clear"></div>
+
+        <div class="para1">
+
+
+
+            <p><strong>FEATURES OF DRONES IN AGRICULTURE : </strong></p>
+            <br>
+            <ol type="1">
+                <li><strong>Irrigation Monitoring </strong>: Drones, including hyperspectral, thermal, or
+                    multispectral sensors, recognize areas that are too dry or need improvement by the farmer. Drone
+                    survey helps improve water efficiency and disclose potential pooling/leaks in irrigation by
+                    providing Irrigation monitoring yields calculations of the vegetation index to help realize the
+                    health of crops and emitted heat/energy.</li>
+                <li><strong>Crop health monitoring </strong>: It is crucial to track the health of the vegetation
+                    and spot bacterial/fungal plagues in the early stages. Agriculture drones can see which plants
+                    reflect different amounts of green light and Near-infrared spectroscopy (NIRS) light. This data
+                    helps produce multispectral images to track crop health. Quick monitoring and discoveries of any
+                    defects can help save crops. In circumstances of crop failure, the farmer can also document the
+                    damages for accurate insurance claims.
+                </li>
+                <li><strong>Crop damage assessment </strong>: Agricultural drones fitted along with multispectral
+                    sensors and RGB sensors also detect field areas inflicted by weeds, infections, and pests.
+                    According to this data, the exact amounts of chemicals needed to fight these infestations are
+                    known, and this helps diminish the costs inflicted by the farmer.</li>
+                <li><strong>Field Soil analysis </strong>: The drone survey allows farmers to obtain information
+                    about their land's soil conditions. Multispectral sensors allow seizing data useful for seed
+                    planting patterns, thorough field soil analysis, irrigation, and nitrogen-level management.
+                    Precise Photogrammetry/ 3D mapping permits farmers to analyze their soil conditions thoroughly.
+                </li>
+                <li><strong>Livestock tracking </strong>: The drone survey allows the farmers not to keep track of
+                    their crops only but also monitor the movements of their cattle. Thermal sensor technology helps
+                    find lost animals and detect an injury or sickness. Drones can carry out this function
+                    favorably, and this adds comprehensively to the production of vegetation.</li>
+            </ol>
+        </div>
+        <br>
+        <br>
+        <p class="para1"><br><br>Farmers must understand the entire process. Determination of goals, creating an
+            equilibrium in the drone and software utilized, and being familiar with the principles of using such
+            technology will stand as a challenge. The farmers will inevitably need comprehensive training or
+            partnerships with third-party experts in the drone industry for the acquisition of reliable data. Drones
+            have changed the course of obtaining data in almost every type of industry, and will only deem to become
+            bigger and better in the coming years.</p>
+
+    </div>
+    <br>
+    <br>
+    <hr style="color:white;height:10px">
+    <br>
+    <br>
+    <div class="container">
+        <p class="ada"><strong>INTERNET OF THINGS</strong></p>
+        <br>
+        <br>
+        <img src="IOT-in-Agriculture.jpg" class="logo1" class="center">
+        <br>
+        <br>
+        <div class="para1">
+            <p><strong>Application of IoT in Agriculture : </strong></p>
+            <br>
+            <ol type="1">
+                <li>IoT-based smart farming is a system built to monitor the agricultural land with the help of sensors
+                    (soil moisture, humidity, light, temperature, etc.) and automate the irrigation practices. The
+                    farmers can observe the field conditions from anywhere. For example, it will alert the farmer when
+                    the soil moisture level is low; the farmer can use sensors to initiate irrigation. IoT-based smart
+                    farming is highly efficient in contrast to traditional methods.
+                </li>
+                <li>Precision farming is the most common application of IoT in farming. It makes the agricultural
+                    practices more precise and well-planned by including processes like real-time crop and soil
+                    condition monitoring, plant health tracking, and weather prediction. The farmers can administer
+                    their fields based on the insights obtained by this system. </li>
+                <li> the IoT-integrated Artificial Intelligence system can give up-to-date alerts and data to optimize
+                    crop growth processes and to reduce yield loss because of pests, detrimental soil moisture level, or
+                    weather damages. This farming method assures precise usage of resources and increases field
+                    productivity while maintaining the sustainability of the process.</li>
+                <li>IoT-equipped irrigation systems not only conserve water but also ensure that crops are getting just
+                    the right amount of water for their optimum growth. This method of irrigation relies on the soil
+                    moisture level rather than pre-determined interval-based irrigation.</li>
+
+            </ol>
+            <br>
+            <br>
+        </div>
+        <div class="para1">
+            <p><strong>ORGANISATIONS INVOLVED IN THE DEVELOPMENT OF THE SYSTEM?</strong></p>
+            <br>
+            <ol>
+                <li>In India, SatSure is a data analytics company that integrates satellite, weather, and IoT analytics
+                    with the agriculture sector to help farmer’s financial security and crop insurance. The company has
+                    mobile apps for giving information on supply statistics of crops and crop stressing in their area.
+                    This aids with deciding what to sow, when to irrigate or add fertilizers, or prepare for harvest.
+                    Currently, the startup’s solutions are used by the Andhra Pradesh State Government.</li>
+                <li>An end-to-end IoT solution for fresh food management, Qzense is employing IoT to develop solutions
+                    for fast and precise grading of fresh food by capturing insights on spoilage, shelf life, and
+                    ripeness, thus helping farmers achieve higher margins from the same produce.</li>
+                </ul>
+                <br>
+                <br>
+        </div>
+        <br>
+        <br>
+        <br>
+
+
+    </div>
+    <br>
+    <br>
+    <hr style="color:white;height:10px">
+    <br>
+    <br>
+    <div class="container">
+        <p class="ada"><strong>KUBOTA TRACTORS</strong></p>
+        <br>
+        <br>
+        <img src="tractor.jpg" class="logo1" class="center">
+        <br>
+        <br>
+        <div class="para1">
+            <p><strong>THE NEED FOR THE TRACTOR : </strong></p>
+            <br>
+            <ol>
+                <li>India is a world leader in agriculture, with about 52%* of the country's land used for farming
+                    (against a world average of about 11%). Producing enough food for India's growing population is
+                    becoming a major issue.
+                </li>
+                <li> As the country develops, the needs for agricultural production to provide the rising population
+                    with a stable food supply and greater yield are increasing every day.
+                </li>
+                <li>Therefore, farmers who once relied on manual labor to tend to their vast fields are now turning to
+                    machinery in the quest for greater efficiency.</li>
+                <li>When the team visited India for the first time, they saw tractors riding all over public roads. The
+                    style in India is to use tractors not only for farming, but also for daily transportation, moving
+                    goods, and a wide range of other uses. Some tractors were riding with heavy cargo spilling off the
+                    trailer, making the front wheels float off the ground.
+                </li>
+                <li>Some were mounted on huge combine harvesters, with the tractor output used as a power source for the
+                    harvesting and running functions of the combine harvester. </li>
+
+                </ul>
+                <br>
+                <br>
+        </div>
+        <!--<div class = "para1">
+        <p><strong>WHICH SECTION OF FARMERS ARE COVERED IN THIS?</strong></p>
+        <br>
+        <ul>
+            <li>All the farmers growing notified crops in a notified area during the season who have insurable in the
+                crop are eligible.</li>
+        </ul>
+        <br>
+        <br>
+        </div>-->
+        <div class="para1">
+            <p><strong>FEATURES OF THE TRACTOR: </strong></p>
+            <br>
+            <ol>
+                <li><STRONG>Powerful and effecient e-CDIS engine : </STRONG>Incorporating a level of precision design
+                    and processing technology that is unique to Kubota 4-valve, Eco-Center Direct Injection System
+                    (e-CDIS) technology is responsible not only for a significant increase in air intake/discharge
+                    efficiency through the use of the 4-valve per cylinder configuration, but also for an notable mixed
+                    air compression performance within the cylinders.</li>
+                <li><strong>4 valves system : </strong>Better combustion and power.
+                </li>
+                <li><strong>Balancer Shaft : </strong>Less vibration and Low noise.</li>
+                <li><strong>Synchronised transmission : </strong>Allows smooth changing of gears .</li>
+
+            </ol>
+        </div>
+        <br>
+        <br>
+
+
+    </div>
+    <br>
+    <br>
+    <footer>
+
+        <p><button class="prev"><a href="new.php">Previous Page</a></button></p>
+
+      </footer>
+
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+        crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    -->
+</body>
+
+</html>
